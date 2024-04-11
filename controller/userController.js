402a3af31d8b30ec1298e09b4abdf9b394052fe9
Biresh <js.biresh@gmail.com>
2024-04-11 17:07:19 +0530

    added jwt feature

--- controller/userController.js
+++ controller/userController.js
@@ -1,7 +1,8 @@
+const generateToken = require("../config/jwtToken");
 const User = require("../models/UserModel");
-const asyncHandler = require('express-async-handler')
+const asyncHandler = require("express-async-handler");
 
-
+// For User creation
 const createUser = asyncHandler(async (req, res) => {
   const email = req.body.email;
   const findUser = await User.findOne({ email: email });
@@ -15,21 +16,57 @@ const createUser = asyncHandler(async (req, res) => {
     //     msg: 'User Already Exists',
     //     success: false
     // })
-    throw new Error('User Already Exists')
+    throw new Error("User Already Exists");
   }
-})
+});
 
-
+// For Login Purposes
 const loginUserControl = asyncHandler(async (req, res) => {
-    const { email, password } = req.body;
-    // console.log(email, password); 
-    // check if user exists or not
-    const findUser = await User.findOne({ email: email })
-    if (findUser && await findUser.isPasswordMatch(password)) {
-        res.json(findUser);
-    } else {
-        throw new Error('Invalid Credentials.')
+  const { email, password } = req.body;
+  // console.log(email, password);
+  // check if user exists or not
+  const findUser = await User.findOne({ email: email });
+  if (findUser && (await findUser.isPasswordMatch(password))) {
+    // res.json(findUser);
+    res.json({
+        _id: findUser?._id,
+        firstname: findUser?.firstname,
+        lastname: findUser?.lastname,
+        email: findUser?.email,
+        mobile: findUser?.mobile,
+        token: generateToken(findUser?._id)
+
+    })
+    // const responseBody = {
+    //   _id: findUser?._id,
+    //   firstname: findUser?.firstname,
+    //   lastname: findUser?.lastname,
+    //   email: findUser?.email,
+    //   mobile: findUser?.mobile,
+    //   token: generateToken(findUser?._id),
+    // };
+
+    // res.json(responseBody);
+    // console.log(responseBody); // Log the response body
+  } else {
+    throw new Error("Invalid Credentials.");
+  }
+});
+
+
+
+// GET all Users 
+const getAllUsers = asyncHandler(async (req, res) => {
+    
+    try {
+        const getUsers = await User.find();
+        res.json(getUsers);
+    } catch (error) {
+        throw new Error(error);        
     }
+
+
+    
 })
 
-module.exports = { createUser, loginUserControl };
+module.exports = { createUser, loginUserControl, getAllUsers };
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const {createUser, loginUserControl} = require("../controller/userController");
+const {createUser, loginUserControl, getAllUsers} = require("../controller/userController");
 const router = express.Router();
 
 router.post("/register", createUser);
 router.post("/login", loginUserControl);
+router.post("/all-users", getAllUsers);
 
 module.exports = router;
